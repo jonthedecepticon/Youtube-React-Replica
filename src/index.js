c0ee1261b5b7998f90a6de8208379e2113999d34
Jon Lambson <jon@crowdengine.com>
2017-04-23 18:11:34 -0600

    youtube search api added and mostly wired up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import { API_KEY } from './Keys';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const App = () => {
-  return <div>HELLO</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'Jon Olsson' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
